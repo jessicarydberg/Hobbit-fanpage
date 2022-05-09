9005dd61113ab27ee17ca9acb6642615a2070ddd
jessicarydberg <jessica.rydberg@hotmail.se>
2022-05-09 13:04:29 +0000

    Add member.html page and links to it

--- run.py
+++ run.py
@@ -21,6 +21,17 @@ def about():
         )
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json.data:
+        data = json.load(json.data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
